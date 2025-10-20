054da2853f5bf7e3aa6153183131f380a15962f3
Prabesh01 <prabesh01@pm.me>
2025-10-20 19:09:15 +0545

    box-chill: sun/mon@4

--- app/utils/info_fetcher.py
+++ app/utils/info_fetcher.py
@@ -1,7 +1,14 @@
 import requests
 
+def get_json(url):
+    return requests.get(url).json()
+
 def get_sbs_chill():
-    r=requests.get("https://fos.sbs.com.au/web/audio/current-song/sbs-chill?delay=90").json()
-    try: return r['songDisplayName']
-    except: return "N/A"
+    r=get_json("https://fos.sbs.com.au/web/audio/current-song/sbs-chill?delay=90")
+    print(r)
+    return r['songDisplayName']
    
+def get_box_chill():
+    r=get_json("https://prod-api.radioapi.me/metadata/1ae42584-729c-4f36-9f8f-f0be92a95bff")
+    return f"{r['song']} by {r['artist']}"
+    
